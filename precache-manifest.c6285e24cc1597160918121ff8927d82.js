5ebd0095b27f026e1f52bbab81a31dca8728f564
LightningsFury <light@memeware.net>
2019-10-28 21:18:50 +0000

    first commit

---
+++ precache-manifest.c6285e24cc1597160918121ff8927d82.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ccc14318fc7c08e3313c81fd289213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ee5414ae1b8bd9f7bb",
+    "url": "/static/js/2.cd343288.chunk.js"
+  },
+  {
+    "revision": "9ab4bc4f86beef101b3e",
+    "url": "/static/js/main.214ce119.chunk.js"
+  },
+  {
+    "revision": "ec9bd5859a081ef6bd4e",
+    "url": "/static/js/runtime-main.00b66737.js"
+  }
+]);
